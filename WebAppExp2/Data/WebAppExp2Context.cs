c966d6cf6306efb63afc1f4a61a24561ed0cb17e
ScatteredDice <scattereddice@gmail.com>
2019-06-06 09:08:01 -0400

    first commit

---
+++ WebAppExp2/Data/WebAppExp2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppExp2.Models
+{
+    public class WebAppExp2Context : DbContext
+    {
+        public WebAppExp2Context (DbContextOptions<WebAppExp2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppExp2.Models.Movie> Movie { get; set; }
+    }
+}
